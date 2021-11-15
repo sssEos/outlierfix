8cb45e80481ba02074e1a6d912a9bdb80585d1b3
sssEos <441592321@qq.com>
2021-11-15 11:16:42 +0800

    uodate

--- R/fixout_conti.R
+++ R/fixout_conti.R
@@ -36,7 +36,7 @@ fixout_conti <-
     # if (is.element("data.table", class(dt)) == FALSE)
     dt = as.data.table(dt)
     # overwirte ----detect type
-    if (length(unique(dt[, which(colnames(dt)==col_name),with=FALSE])) < 10) {
+    if (length(unique(dt[, which(colnames(dt)==eval(col_name)),with=FALSE])) < 10) {
       warning("The data does not seem to be continuous or have very few observations.")
     }
     if (interactive == TRUE) {
